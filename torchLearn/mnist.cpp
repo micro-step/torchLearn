3c3ebcbf091c2d907429312be702519488e0a22e
juzi <472147797@qq.com>
2019-01-28 17:48:00 +0800

    add mnist

--- torchLearn/mnist.cpp
+++ torchLearn/mnist.cpp
@@ -1,112 +1,86 @@
+#include "mnist.h"
+
+#include <torch/torch.h>
+
 #include <cstddef>
 #include <iostream>
-#include <striing>
+#include <string>
 #include <vector>
 
-template <typename DataLoader>
-void train(
-	int32_t epoch,
-	const Options& options,
-	Net& model,
-	torch::Device device,
-	DataLoader& data_loader,
-	torch::optim::SGD& optimizer,
-	size_t dataset_size)
-{
-	model.train();
-	size_t batch_idx = 0;
-	for (auto& batch:data_loader)
-	{
-		auto data = batch.data.to(device), targets = batch.target.to(device);
-		optimizer.zero_grad();
-		auto output = model.forward(data);
-		auto loss = torch::nll_loss(output, targets);
-
-		loss.backward();
-		optimizer.step();
-
-		if (batch_idx++%optimizer.log_interval==0)
-		{
-			std::cout << "Train Epoch: " << epoch << "["
-				<< batch_idx*batch.DataLoader.size(0) << "/" << dataset_size
-				<< "]\t Loss: " << loss.template item <float>() << std::endl;
-		}
-
-	}
-}
-
 template<typename DataLoader>
-vodi test(
-	Net& model,
-	torch::Device device,
-	DataLoader& data_loader,
-	size_t dataset_size){
-	torch::NoGradGuard no_grad;
-	model.eval();
-	double test_loss = 0;
-	int32_t correct = 0;
-	for(const auto& batch : data_loader){
-		auto data = batch.data.to(device), target = batch.target.to(device);
-		auto output model.forward(data);
-
-		tost_loss += torch::nll_loss(
-			output,
-			target,
-			{},
-			Reduction::Sum)
-			.template item<float>();
-		auto pred = output.argmax(1);
-		correct += pred.eq(target).sum().template item<int64_t>();
-	}
-	test_loss /= dataset_size;
-	std::cout << "Test set :Average loss: " << test_loss 
-		<< ",Accuracy£º" << correct << "/" << dataset_size << std::endl;
+void train(
+    int32_t epoch,
+    const Options& options,
+    Net& model,
+    torch::Device device,
+    DataLoader& data_loader,
+    torch::optim::SGD& optimizer,
+    size_t dataset_size){
+    model.train();
+    size_t batch_idx=0;
+    for(auto& batch: data_loader)
+    {
+        auto data=batch.data.to(device),targets=batch.target.to(device);
+        optimizer.zero_grad();
+        auto output=modle.forward(data);
+        auto loss=torch::nll_loss(output ,targets);
+        loss.backward();
+        optimizer.step();
+        if (batch_idx++ % options.log_interval==0) {
+            std::cout<<"Train Epoch: "<<epoch<<"[ "
+            <<batch_idx*batch.data.size(0)<<"/"<<dataset_size
+            <<"] \t loss: "<<loss.template item<float>()<<std::endl;
+        }
+    }
 }
-auto main(int argc, const char* argv[]){
-	torch::manual_seed(0);
-	Option options;
-	torch::DeviceType device_type;
-	if (torch::cuda::is_available()& != options.no_cuda)
-	{
-		std::cout << "CUDA available! Training on GPU" << std::endl;
-		device_type = torch::kCUDA;
-	}
-	else
-	{
-		std::cout << "Traning on CPU" << std::endl;
-		device_type = torch::kCPU;
-	}
 
-	torch::Device device(device_type);
-	Net model;
-	model.to(device);
+template <typename DataLoader>
+void test(
+    Net& model,
+    torch::Device device,
+    DataLoader& data_loader,
+    size_t dataset_size){
+    torch::NoGradGuard no_grad;
+    model.eval();
 
-	auto train_dataset =
-		torch::data::datasets::MNIST(
-		options.data_root, torch::data::datasets::MNIST::Mode::kTrain)
-		.map(Normalize(0.1307, 0.3081))
-		.map(torch::data::transforms::Stack<>());
-	const auto dataset_size = train_dataset.size();
+    double test_loss=0;
+    int32_t correct=0;
 
-	auto train_loader = torch::data::make_data_loader(
-		std::move(train_dataset), options.batch_size);
+    for(const auto& batch : dataloader)
+    {
+        auto data=batch.data.to(device),targets=batch.target.to(device);
 
-	auto test_loader = torch::data::make_data_loader(
-		torch::data::datasets::MNIST(
-		options.data_root, torch::data::datasets::MNIST::Mode::kTest)
-		.map(Normalize(0.1307, 0.3081))
-		.map(torch::data::transforms::Stack<>()),
-		options.batch_size);
+        auto output= model.forward(data);
+        test_loss+=torch::nll_loss(
+            output,
+            targets,
+            {},
+            Reduction::sum).template item<float>();
 
-	torch::optim::SGD optimizer(
-		model.parameters(),
-		torch::optim::SGDOptions(options.lr).momentum(options.momentum));
+        auto pred=output.argmax(1);
+        correct +=pred.eq(targets).sum().template item<int64_t>();
+        /* code */
+    }
+    teat_loss /=dataset_size;
+    std::cout<< "Test set: Average loss: " <<test_loss
+    <<", Accuracy :" <<correct <<"/" <<dataset_size<<std::endl;
+}
 
-	for (size_t epoch = 1; epoch <= options.epochs; ++epoch) {
-		train(
-			epoch, options, model, device, *train_loader, optimizer, dataset_size.value());
-		test(model, device, *test_loader, dataset_size.value());
-	}
 
-	return 0;
+auto main(int argc,const char* agrv[])->int{
+
+    torch::manual_seed(0);
+    Options options;
+    torch::DeviceType device_type;
+    if (torch::cuda::is_available()&& !options.no_cuda) {
+        std::cout<<"CUDA available! Training on GPU"<<std::endl;
+        device_type=torch::kCUDA;
+    }
+    else{
+        std::cout<<"Training on cpu"<<std::endl;
+        device_type =torch::kCPU;
+    }
+    torch::Device device(device_type);
+    Net model;
+
 }
